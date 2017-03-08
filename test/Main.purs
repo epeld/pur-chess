70ab40b229695004a2ce64b8341a29864c37641a
Erik Peldan <epe@trifork.com>
2017-03-08 22:54:27 +0100

    Add some tests

--- test/Main.purs
+++ test/Main.purs
@@ -4,6 +4,15 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Test.Unit (suite, test, timeout)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+
+
+import Main
+
+--main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = runTest do
+  suite "test1" do
+    test "stuff" do
+      Assert.assert "hello" $ 1 == 1
