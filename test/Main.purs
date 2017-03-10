220707e0a5f497a5101068ae08a10d351757d45a
Erik Peldan <epe@trifork.com>
2017-03-10 22:56:21 +0100

    Write failing test

--- test/Main.purs
+++ test/Main.purs
@@ -1,6 +1,10 @@
 module Test.Main where
 
 import Prelude
+import Data.Maybe
+import Data.Functor
+import Data.String
+import Control.Monad
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
@@ -11,8 +15,20 @@ import Test.Unit.Assert as Assert
 
 import Main
 
+decodeEncodeSquare = parseSquare >=> squareString
+
+assertIdempotSquare sq =
+  let isIdempot s = fromMaybe false (decodeEncodeSquare s <#> \s' -> s == s')          
+  in if isIdempot sq
+     then Assert.assert sq $ true
+     else Assert.assert (joinWith " " [sq, "!=", (fromMaybe "??" (decodeEncodeSquare sq))]) $ false
+
+
 --main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = runTest do
   suite "test1" do
-    test "stuff" do
-      Assert.assert "hello" $ 1 == 1
+    test "decoding, then encoding square is idempotent" do
+      assertIdempotSquare "e4"
+      assertIdempotSquare "g3"
+      assertIdempotSquare "f2"
+      assertIdempotSquare "a8"
